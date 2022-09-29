da5b030e9d9f3127edb31124ce36a6efadcea6b1
Jono Cameron <jacc@bu.edu>
2022-09-29 14:13:24 +0100

    Leetcode hard :(

---
+++ construct_rectangle/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* constructRectangle(int area, int* returnSize){
+    int ;
+
+    //printf("hello, %d, %d", arr[0], arr[1]);
+    return 0;
+}
+
+int main(){
+    int area = 42;
+    int arr[2];
+
+    arr[0] = 1;
+    arr[1] = 2;
+
+    constructRectangle(area, arr);
+    printf("hello, %d, %d", arr[0], arr[1]);
+    return 0;
+}
---
+++ grey_code/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* greyCode(int n, int* returnSize){
+    // do nothing 
+}
+
+int main(){
+    int n = 2;
+
+
+    printf("Too hard for now lol\n");
+    return 0;
+}

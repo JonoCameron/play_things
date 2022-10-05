e4d83687e479de98a8e08175f2fa49def3de8fb2
Jono Cameron <jacc@bu.edu>
2022-10-05 09:51:29 +0100

    Wednesday 5th October

--- construct_rectangle/main.c
+++ construct_rectangle/main.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* constructRectangle(int area, int* returnSize){
-    int ;
+    printf("here\n");
+    int* arr;
+    printf("ohno\n");
+    arr = (int*)malloc(2 * sizeof(int));
+    printf("where");
+    *returnSize = 2;
+    int w = sqrt(area);
+    
+    while(w){
+        if(!(area%w)){
+            *arr = area/w;
+            *(arr + 1) = w;
+            break;
+        }
+    }
 
-    //printf("hello, %d, %d", arr[0], arr[1]);
-    return 0;
+    printf("hello, %d, %d\n", *arr, *(arr + 1));
+    return arr;
 }
 
 int main(){
     int area = 42;
-    int arr[2];
-
-    arr[0] = 1;
-    arr[1] = 2;
-
-    constructRectangle(area, arr);
-    printf("hello, %d, %d", arr[0], arr[1]);
+    int* retSize = 0;
+    printf("there\n");
+    constructRectangle(area, retSize);
+    //printf("hello, %d, %d", arr[0], arr[1]);
     return 0;
 }
